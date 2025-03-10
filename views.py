920f5f5a9d241811b95c701a495c108e58a3c3ab
faiza-anjoom <faizaanjoomohona@gmail.com>
2025-03-10 15:05:27 +0600

    Update views.py

--- views.py
+++ views.py
@@ -4,27 +4,23 @@ from django.urls import reverse_lazy
 from .models import Employee
 from .forms import EmployeeForm
 
-# List employees
 class EmployeeListView(ListView):
     model = Employee
     template_name = 'employees/employee_list.html'
     context_object_name = 'employees'
 
-# Add an employee
 class EmployeeCreateView(CreateView):
     model = Employee
     form_class = EmployeeForm
     template_name = 'employees/employee_form.html'
     success_url = reverse_lazy('employee_list')
 
-# Edit an employee
 class EmployeeUpdateView(UpdateView):
     model = Employee
     form_class = EmployeeForm
     template_name = 'employees/employee_form.html'
     success_url = reverse_lazy('employee_list')
 
-# Delete an employee
 class EmployeeDeleteView(DeleteView):
     model = Employee
     template_name = 'employees/employee_confirm_delete.html'
