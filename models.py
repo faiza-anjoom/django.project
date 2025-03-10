bbc293b3a03ebb336b41ca2a405ee59ccafae6fa
faiza-anjoom <faizaanjoomohona@gmail.com>
2025-03-10 15:01:07 +0600

    files added

---
+++ models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    department_name = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
